9dff0d0502bf3a8e6662547fce23245bad1ac43a
Emil Gustafsson <emiltgustafsson@hotmail.com>
2020-02-15 20:26:54 +0100

    Update sprites

---
+++ WGJ136/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Goal redGoal;
+    public Goal blueGoal;
+
+    private void FixedUpdate()
+    {
+        if (redGoal.onPlayer && blueGoal.onPlayer)
+        {
+            Debug.Log("Game Won");
+        }
+    }
+}
---
+++ WGJ136/Assets/Scripts/Goal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public float collisionRadius = 0.1f;
+    public LayerMask playerLayer;
+    public bool onPlayer = false;
+
+    private void Start()
+    {
+        InvokeRepeating("Rotate", 1f, 1f);
+    }
+
+    void FixedUpdate()
+    {
+        onPlayer = Physics2D.OverlapCircle((Vector2) transform.position, collisionRadius, playerLayer);
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(0,0,90);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        var position = transform.position;
+        
+        //var positions = new Vector2[] {bottomOffset, leftOffset, rightOffset};
+        Gizmos.DrawWireSphere((Vector2)transform.position , collisionRadius);
+    }
+}
